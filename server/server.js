803a14643f7569b75150f41ec718549462ab8b52
Sam Trapskin <samtrapskin@gmail.com>
2018-04-19 11:39:49 -0500

    dummy get test

--- server/server.js
+++ server/server.js
@@ -2,13 +2,14 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var port = process.env.PORT || 5000;
+const pizzaRouter = require('./routes/pizza.router');
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
 app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
-
+app.use('/pizza', pizzaRouter)
 
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
--- src/components/MenuDummy.js
+++ src/components/MenuDummy.js
@@ -1,10 +1,43 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
 
 class MenuDummy extends Component {
+    
+    constructor (props){
+        super(props)                                                          //3.
+      
+        this.state= {
+          pizzaItem: [],
+          
+        }
+    }
+     
+
+    getPizzas() {
+        axios.get('/pizza')
+        .then((response) => {
+            this.setState({                                                 //4.
+                pizzaItem: response.data      
+                });     
+                console.log(this.state.pizzaItem)
+        })
+        .catch((error) => {
+        console.log('Error,in GET', error) 
+        })   
+    }
+    
+    componentDidMount() {
+        this.getPizzas(); 
+    }
+
+
+
     render(){
         return(<h1>MenuDummy</h1>)
     }
-
 }
+
 
 export default MenuDummy;
