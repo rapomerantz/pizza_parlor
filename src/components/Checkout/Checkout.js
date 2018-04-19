4453df457857b246b59ddf7d663476fae1a3bc16
Atticus Pomerantz <rapomerantz@gmail.com>
2018-04-19 10:38:33 -0500

    All router links made

---
+++ src/components/Checkout/Checkout.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Checkout extends Component {
+
+  render() {
+
+    return (
+        //THIS DIV HOLDS THE ENTIRE CHECKOUT VIEW
+      <div>
+        <header>
+          <h1>Checkout</h1>
+        </header>
+        <hr/>
+        <h2>THIS IS THE CHECKOUT VIEW</h2>
+      </div>
+    );
+  }
+}
+
+export default Checkout;
---
+++ src/components/Orders/Orders.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Orders extends Component {
+
+  render() {
+
+    return (
+        //THIS DIV HOLDS THE ENTIRE ORDERS VIEW
+      <div>
+        <header>
+          <h1>Orders</h1>
+        </header>
+        <hr/>
+        <h2>THIS IS THE ORDERS VIEW</h2>
+      </div>
+    );
+  }
+}
+
+export default Orders;
