95e7be5515f02c4fb1a2ff2b3f9a8cba1ab53eff
Atticus Pomerantz <rapomerantz@gmail.com>
2018-04-19 17:03:36 -0500

    Began work on Redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,41 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers, applyMiddleware } from 'redux'; 
+import { Provider } from 'react-redux'; 
+import axios from 'axios'; 
+import logger from 'redux-logger' 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const pizzaReducer = (state = {splat: 0, Onamonapizza: 0, Pepperoni: 0, Rainbow: 0, Firedragon: 0, Date: 0}, action) => {
+    console.log('in pizzaReducer, state:', state, 'action:', action);
+    switch(action.type) { 
+        default:
+            console.log('SPLAT + 1');
+            state.splat +1;
+            console.log(state);
+            return state; 
+    }
+}
+
+
+
+
+
+
+
+
+const storeInstance = createStore(
+    combineReducers({
+        //reducers go here
+        pizzaReducer
+    }),
+    //middleware goes here
+        applyMiddleware(logger),
+
+
+)
+
+
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
