d6c40f5e6d690aab855a2a54dbd363706eba0135
Sam Trapskin <samtrapskin@gmail.com>
2018-04-19 16:43:55 -0500

    working on orders GET

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var port = process.env.PORT || 5000;
 const pizzaRouter = require('./routes/pizza.router');
+const orderRouter = require('./routes/')
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
@@ -10,6 +11,7 @@ app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
 app.use('/pizza', pizzaRouter)
+app.use('/orders',orderRouter)
 
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
